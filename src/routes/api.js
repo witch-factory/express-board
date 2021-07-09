92bda1c289000ed6dca7359503c73245384f65b9
witch-factory <soakdma37@gmail.com>
2021-07-09 23:54:40 +0900

    회원가입 구현중

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,8 +1,10 @@
 import express from "express";
 import post from "./post.js";
+import auth from "./auth";
 
 const router=express.Router();
 
 router.use("/post", post);
+router.use("/auth", auth);
 
 export default router;
---
+++ src/routes/auth.js
@@ -0,0 +1,11 @@
+import express from "express";
+import mysql from "../db/mysql.js";
+
+const router=express.Router();
+
+router.post("/new", (req, res)=>{
+  console.log(req.body);
+  return res.json({
+    success:"ok"
+  })
+})
