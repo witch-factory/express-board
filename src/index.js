9bdb136ac645b62956e74d83373c93e19e6257fd
witch-factory <soakdma37@gmail.com>
2021-07-15 10:35:19 +0900

    세션 로그인 구현중. session작동까지 확인

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import api from "./routes/api.js";
 import conn from "./db/mysql.js";
+import session from "express-session";
+import session_opts from "./session/options.js";
+
 
 const app=express();
 let PORT=8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(session(session_opts));
 app.use("/api", api);
 
 let sqlQuery="select * from user";
@@ -16,6 +20,14 @@ conn.query(sqlQuery, (err,results,fields)=>{
   if(err){console.log(err);}
   console.log(results);
 });
+
+app.get("/test", (req, res)=>{
+  req.session.test=1;
+  console.log(req.body);
+  return res.json({
+    session:req.session.test
+  })
+})
 
 app.listen(PORT, ()=>{
   console.log("Express listening on port", PORT);
---
+++ src/session/options.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const options={
+  secret:process.env.SESSION_SECRET,
+  resave:false,
+  saveUninitialized:true
+}
+
+export default options;
