b0d451040d78809ed827b40a7de24d835650b4c4
Kim Sung Hyun <soakdma37@gmail.com>
2021-07-16 11:51:49 +0900

    세션 연동 완료

--- src/index.js
+++ src/index.js
@@ -3,18 +3,27 @@ import bodyParser from "body-parser";
 import api from "./routes/api.js";
 import conn from "./db/mysql.js";
 import session from "express-session";
-import session_opts from "./session/options.js";
-import MySQLSessionStore from "express-mysql-session";
+import session_store_opts from "./session/options.js";
+import expressMySQLSession from "express-mysql-session";
 
-
-
+const MySQLStore=expressMySQLSession(session);
 
 const app=express();
+const sessionStore=new MySQLStore(session_store_opts);
+
 let PORT=8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(session(session_opts));
+app.use(
+  session({
+    key:'session-cookie-name',
+    secret:'session-cookie-secret',
+    store:sessionStore,
+    resave:false,
+    saveUninitialized:false
+  })
+);
 app.use("/api", api);
 
 let sqlQuery="select * from user";
--- src/session/options.js
+++ src/session/options.js
@@ -3,9 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const options={
-  secret:process.env.SESSION_SECRET,
-  resave:false,
-  saveUninitialized:true
-}
+  host:'localhost',
+  port:3306,
+  user:'root',
+  password:'zookim97',
+  database:'simplebbs'
+};
 
 export default options;
