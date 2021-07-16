95eaa8cd6d3a5d0cc0dec281094a2e3f7546f642
Kim Sung Hyun <soakdma37@gmail.com>
2021-07-16 21:15:15 +0900

    게시판 불러오기 기능 완료. mysql 데이터로 테스트해야

--- src/routes/board.js
+++ src/routes/board.js
@@ -1,7 +1,58 @@
 import express from "express";
-import mysql from "../db/mysql";
+import mysql from "../db/mysql.js";
 
 const router=express.Router();
+
+router.get("/:id", (req, res)=>{
+  if(!req.session.token){
+    return res.json({
+      ok:false,
+      status:404,
+      error:"unauthorized author"
+    });
+  }
+
+  const bbsID=req.params.id;
+  const sqlQuery="select b.id, b.title, b.content, u.username " +
+    "from board as b inner join user as u on b.writer=u.id where b.id=?";
+
+  const post=[bbsID];
+
+  mysql.query(sqlQuery, post, (err,results,fields)=>{
+    if(err){
+      console.log(err);
+      return res.json({
+        ok:false,
+        status:400,
+        error:"db error"
+      })
+    }
+
+    const board=results[0];
+    const sqlQuery="select c.id, c.message, u.username " +
+      "from comment as c inner join user as u on c.writer=u.id" +
+      "inner join board as b on c.board=b.id where b.id=?";
+
+    const post=[bbsID];
+
+    mysql.query(sqlQuery, post, (err,results, fields)=>{
+      if(err){
+        console.log(err);
+        return res.json({
+          ok:false,
+          status:400,
+          error:"db error 2 in comment query"
+        });
+      }
+
+      const comments=results;
+      return res.json({
+        board,
+        comments
+      })
+    })
+  })
+})
 
 router.post("/", async(req, res)=>{
   const username=req.session.token;
