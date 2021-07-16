1388701b31778983249907b7958b99c1d50809e5
Kim Sung Hyun <soakdma37@gmail.com>
2021-07-16 20:34:11 +0900

    게시판 불러오기 기능을 위한 준비

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,14 @@
 import express from "express";
 import post from "./post.js";
 import auth from "./auth.js";
+import board from "./board.js";
+import boardlist from "./boardlist.js";
 
 const router=express.Router();
 
 router.use("/post", post);
 router.use("/auth", auth);
+router.use("/board", board);
+router.use("/boardlist", boardlist);
 
 export default router;
---
+++ src/routes/boardlist.js
@@ -0,0 +1,10 @@
+import express from "express";
+import mysql from "../db/mysql.js";
+
+const router=express.Router();
+
+router.get("/", (req, res)=>{
+
+})
+
+export default router;
