5f8b8a89c661b021a23d14e65554317cc4c50e55
witch-factory <soakdma37@gmail.com>
2021-07-17 11:11:01 +0900

    server에서 클라이언트로 접속되게 세팅

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,9 @@ import api from "./routes/api.js";
 import session from "express-session";
 import session_store_opts from "./session/storeOptions.js";
 import expressMySQLSession from "express-mysql-session";
+import path from 'path';
 
+const __dirname=path.resolve();
 const MySQLStore=expressMySQLSession(session);
 
 const app=express();
@@ -24,6 +26,9 @@ app.use(
   })
 );
 app.use("/api", api);
+
+app.use("/", express.static(__dirname+"../../client/build"));
+//클라이언트 빌드를 정적 파일로 지정
 
 app.get("/test", (req, res)=>{
   req.session.test=1;
