65d89ee91eddd07449b8a0d0e488a9c7dd49e94d
witch-factory <soakdma37@gmail.com>
2021-07-25 01:09:28 +0900

    익스프레스 공식문서

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
-const express=require('express');
+import express from "express";
+
 const app=express();
 const port=3000;
 
@@ -8,4 +9,4 @@ app.get('/', (req, res)=>{
 
 app.listen(port, ()=>{
   console.log(`example in localhost ${port}`);
-  })
+})
