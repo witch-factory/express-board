99fb5e149eb8d0699675438ca7033ff03bafc8a1
witch-factory <soakdma37@gmail.com>
2021-07-25 23:25:53 +0900

    미들웨어 함수

--- server/app.js
+++ server/app.js
@@ -1,15 +1,20 @@
 import express from "express";
-import birds from "./birds.js";
 
 const app=express();
 const port=3000;
 
+const requestTime=(req,res,next)=>{
+  req.requestTime=Date.now();
+  //요청의 타임스탬프를 표시
+  next();
+}
+
+app.use(requestTime);
+
 app.get('/', (req, res)=>{
-  res.send('Hello World');
+  const responseText='Requested at '+req.requestTime+'';
+  res.send(responseText);
 })
 
-app.use('/birds', birds);
 
-app.listen(port, ()=>{
-  console.log(`example in localhost ${port}`);
-})
+app.listen(port);
